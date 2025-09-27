ecd523e027d8db9c4bcb9dc823f914e75a97b0d9
sonsungjong <tbxmtbfm@naver.com>
2025-09-27 10:00:58 +0900

    next

---
+++ src/pages/api/auth/[...nextauth].js
@@ -0,0 +1,72 @@
+// npm install next-auth
+// npm install bcrypt
+// npm i --save-dev @types/bcrypt
+import NextAuth, { NextAuthOptions } from "next-auth";
+import GithubProvider from "next-auth/providers/github"
+import GoogleProvider from "next-auth/providers/google"
+import CredentialsProvider from "next-auth/providers/credentials";
+import bcrypt from "bcrypt";            // 비밀번호 암호화용
+import { connectDB } from "@/Utils/db";
+
+// 구글로그인 OAuth
+// 깃허브로그인 OAuth
+// 자체DB로그인
+const googleId = process.env.GOOGLE_ID || "";
+const googleSecret = process.env.GOOGLE_SECRET || "";
+const nextauthSecret = process.env.NEXTAUTH_SECRET || "siwasoftweb";
+const DBName = process.env.DB_NAME || "siwasoftweb";
+
+export const authOptions = {
+    providers: [
+        GoogleProvider({
+            // 구글 로그인
+            clientId: googleId || "",
+            clientSecret: googleSecret || ""
+        }),
+        CredentialsProvider({
+            // 자체DB 로그인
+            name : "credentials",
+            credentials:{
+                email:{label:"email", type:"text"},
+                password:{label:"password", type:"password"}
+            },
+            
+            // 로그인 시도
+            async authorize(credentials){
+                let db = (await connectDB).db(DBName)
+                let user = await db.collection('user').findOne({email:credentials?.email});
+                if(!user) return null;
+
+                const ok = await bcrypt.compare(credentials?.password ?? "", user.password);
+                if(!ok) return null;
+
+                return{
+                    id:String((user)._id),
+                    name:(user).name,
+                    email:(user).email
+                 }
+            }
+        }),
+    ],
+    session:{
+        strategy:'jwt',
+        maxAge: 2 * 60 * 60
+    },
+    callbacks:{
+        jwt: async({token, user}) =>{
+            if(user){
+                token.user={};
+                token.user.name = user.name;
+                token.user.email = user.email;
+            }
+            return token;
+        },
+        session: async({session, token})=>{
+            session.user = token.user;
+            return session;
+        }
+    },
+    secret:nextauthSecret
+}
+
+export default NextAuth(authOptions);
---
+++ src/pages/api/auth/signup.js
@@ -0,0 +1,32 @@
+import bcrypt from 'bcrypt'
+import { connectDB } from '@/Utils/db'
+
+const dbName = process.env.DB_NAME || "siwasoftweb";
+
+export default async function handler(req, res)
+{
+    res.setHeader('Access-Control-Allow-Origin', "*");
+    res.setHeader('Access-Control-Allow-Methods', 'GET,POST,PUT,DELETE,OPTIONS');
+    res.setHeader('Access-Control-Allow-Headers', 'Content-Type, Authorization');
+
+    if(req.method === "OPTIONS"){
+        res.status(200).end();
+        return;
+    }
+
+    try{
+        console.log('/api/auth/signup')
+        if(req.method === "POST"){
+            let hash = await bcrypt.hash(req.body?.password, 10);       // DB에 암호화해서 저장
+            req.body.password = hash;       // 암호화한 것으로 교체
+
+            let db = (await connectDB).db(dbName)
+            await db.collection('user').insertOne(req.body);
+            res.redirect(302, '/api/auth/signin');       // 회원가입 됬으면 로그인 페이지로 돌아가게
+            return;             // 변경
+        }
+    }catch(error){
+        res.status(500).json({error:'signup failed: '+error})
+        return;                 // 변경
+    }
+}
